7c6b4c18407dbf9b19283f31f429cdc12d11de22
Samantha Lurio <samantha.lurio@gmail.com>
2020-07-06 15:02:52 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,13 +1,6 @@
 require 'pry'
 
-def generate_star_date
-  (rand(100000) + 400000) / 10.0
-end
 
-
-def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
-end
 
 def engage
   puts state_log(date)
